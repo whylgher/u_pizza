5e38660cf21cb4b694bd408a74233d4e8dd6229a
Whyll <whylgher@gmail.com>
2023-01-01 17:32:15 +0000

    conexão com back-end registro firebase e login com email e senha

--- lib/app/service/user/user_service_impl.dart
+++ lib/app/service/user/user_service_impl.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 import './user_service.dart';
 import '../../core/exception/failure.dart';
+import '../../core/exception/user_exists_exception.dart';
 import '../../core/logger/app_logger.dart';
 import '../../repositories/user/user_repository.dart';
 
@@ -19,21 +20,21 @@ class UserServiceImpl implements UserService {
   Future<void> register(
       String email, String password, String name, String phone) async {
     try {
-      // final firebaseAuth = FirebaseAuth.instance;
+      final firebaseAuth = FirebaseAuth.instance;
 
-      // final userLoggedMethods =
-      //     await firebaseAuth.fetchSignInMethodsForEmail(email);
+      final userLoggedMethods =
+          await firebaseAuth.fetchSignInMethodsForEmail(email);
 
-      // if (userLoggedMethods.isNotEmpty) {
-      //   throw UserExistsException();
-      // }
+      if (userLoggedMethods.isNotEmpty) {
+        throw UserExistsException();
+      }
 
       await _userRepository.register(email, name, password, phone);
 
-      // final userRegisterCredential = await firebaseAuth
-      //     .createUserWithEmailAndPassword(email: email, password: password);
+      final userRegisterCredential = await firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: password);
 
-      // await userRegisterCredential.user?.sendEmailVerification();
+      await userRegisterCredential.user?.sendEmailVerification();
     } on FirebaseException catch (e, s) {
       _log.error('Erro ao criar usuário no Firebase', e, s);
       throw Failure(message: 'Erro ao criar usuário.');
