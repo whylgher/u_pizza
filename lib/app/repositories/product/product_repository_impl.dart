84f604113db52f585d6102c068625b23ae9541a2
Whyll <whylgher@gmail.com>
2023-01-06 17:46:27 +0000

    fix list view item

--- lib/app/repositories/product/product_repository_impl.dart
+++ lib/app/repositories/product/product_repository_impl.dart
@@ -1,17 +1,31 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
 import './product_repository.dart';
+import '../../core/logger/app_logger.dart';
 import '../../core/rest_client/rest_client.dart';
+import '../../core/ui/widgets/messages.dart';
 
 class ProductRepositoryImpl implements ProductRepository {
   final RestClient _restClient;
+  final AppLogger _log;
 
   ProductRepositoryImpl({
     required RestClient restClient,
-  }) : _restClient = restClient;
+    required AppLogger log,
+  })  : _restClient = restClient,
+        _log = log;
 
   @override
   Future<Map<String, dynamic>> getPizza(int id) async {
-    String url = '/api/pizza/$id';
-    final result = await _restClient.auth().get(url);
-    return result.data;
+    try {
+      String url = '/api/pizza/$id';
+      final result = await _restClient.auth().get(url);
+      return result.data;
+    } on Exception catch (e) {
+      Modular.to.navigate('/auth/login');
+      _log.error('Usuário não logado', e);
+      Messages.alert('You must be logged.');
+      throw Exception;
+    }
   }
 }
