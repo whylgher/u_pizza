e7a08238338ba4dd90442fbfc517857baa49c6b6
Whyll <whylgher@gmail.com>
2023-01-08 13:29:38 +0000

    fix controller

--- lib/app/modules/auth/product/controller/product_controller.dart
+++ lib/app/modules/auth/product/controller/product_controller.dart
@@ -15,25 +15,29 @@ abstract class ProductControllerBase with Store {
   }) : _productService = productService;
 
   @observable
-  ObservableList<dynamic> pizza = ObservableList<dynamic>.of([]);
-
+  Map pizza = {};
+  @observable
+  List<dynamic> borders = [];
+  @observable
+  List<dynamic> additionals = [];
   @observable
   double price = 0;
-
   @observable
   double priceAdditional = 0;
-
   @observable
   double priceBorder = 0;
-
   @observable
   int itemAdditional = 0;
-
   @observable
   late double priceTotaly = price;
-
   @observable
   int item = 1;
+  @observable
+  bool cutPizza = true;
+  @observable
+  bool regular = true;
+  @observable
+  bool large = false;
 
   @action
   void increment() {
@@ -61,9 +65,6 @@ abstract class ProductControllerBase with Store {
     priceAdditional = priceAdditional - value;
   }
 
-  @observable
-  bool cutPizza = true;
-
   @action
   void cutInHalf() {
     cutPizza = true;
@@ -73,11 +74,6 @@ abstract class ProductControllerBase with Store {
   void notCutPizza() {
     cutPizza = false;
   }
-
-  @observable
-  bool regular = true;
-  @observable
-  bool large = false;
 
   @action
   void sizePizzaLarge() {
@@ -96,13 +92,16 @@ abstract class ProductControllerBase with Store {
     priceAdditional = 0;
     priceBorder = 0;
     itemAdditional = 0;
+    item = 1;
     priceTotaly = price * item;
   }
 
   @action
   Future<void> getPizza(int id) async {
     var data = await _productService.getPizza(id);
-    pizza = ObservableList<dynamic>.of([data]);
+    pizza = data['pizza'];
+    additionals = data['additionals'];
+    borders = data['borders'];
     // Loader.show();
   }
 }
