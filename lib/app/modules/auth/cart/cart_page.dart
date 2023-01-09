9288a1631ea65ce2fa78651d9d1dc7cc0ed6fc45
Whyll <whylgher@gmail.com>
2023-01-09 15:56:58 +0000

    cart pag items

--- lib/app/modules/auth/cart/cart_page.dart
+++ lib/app/modules/auth/cart/cart_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 import '../../../core/ui/extensions/size_screen_extension.dart';
 import '../../../core/ui/icons/u_pizza_icons.dart';
 import '../../../core/ui/widgets/app_bar_default_widget.dart';
+import '../product/controller/product_controller.dart';
 
 part 'widget/add_drinks.dart';
 part 'widget/address_paymen_coupon_widget.dart';
@@ -17,6 +18,7 @@ class CartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Modular.get<ProductController>();
     return Scaffold(
       backgroundColor: const Color(0xFFF7F8FA),
       appBar: AppBarDefaultWidget(
@@ -25,13 +27,28 @@ class CartPage extends StatelessWidget {
           Modular.to.navigate('/auth/product_page');
         },
       ),
-      bottomNavigationBar: const BottomNavigationBarWidget(),
+      bottomNavigationBar: BottomNavigationBarWidget(),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
             children: [
               const AddressPaymenCouponWidget(),
-              const ItensWidget(),
+              ListView.separated(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: controller.cardList.length,
+                separatorBuilder: (_, __) => Divider(height: 20.h),
+                itemBuilder: (context, index) {
+                  return ItensWidget(
+                    name: controller.cardList[index].name,
+                    description: controller.cardList[index].description,
+                    item: (index + 1).toString(),
+                    unitys: controller.cardList[index].amountPizzas,
+                    image: controller.cardList[index].img,
+                    total: controller.cardList[index].amount,
+                  );
+                },
+              ),
               SizedBox(
                 height: 15.h,
               ),
